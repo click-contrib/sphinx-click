c1afb68d834dba25d4c729a8941a54b127cdb38b
Stephen Finucane <stephen@that.guru>
2022-04-27 10:37:58 +0100

    docs: Add root directory to PATH for docs build
    Signed-off-by: Stephen Finucane <stephen@that.guru>

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,7 @@
 import os
 import sys
 
+sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../examples'))
 
 # -- General configuration ------------------------------------------------
