70466cfef4d8baf64bd8dbe64ce276230919458e
dylanpilz <dpilz@ucsd.edu>
2024-03-14 09:15:28 -0700

    linting

--- sphinx_click/ext.py
+++ sphinx_click/ext.py
@@ -82,7 +82,7 @@ def _get_help_record(ctx: click.Context, opt: click.Option) -> ty.Tuple[str, str
     if opt.show_default is not None:
         show_default = opt.show_default
     else:
-        show_default =  ctx.show_default
+        show_default = ctx.show_default
 
     if isinstance(show_default, str):
         # Starting from Click 7.0 show_default can be a string. This is
